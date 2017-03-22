74f2ca49bdb74aee922c6adbeed9a70eac607e50
Gorgoras <martinzurita1@gmail.com>
2017-03-22 16:14:50 -0300

    Trying to commit with git|

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 account = input("Nombre de la cuenta: ")
@@ -31,38 +30,3 @@ if not os.path.exists(carpeta):
 #    directory = os.path.dirname(file_path)
 #    if not os.path.exists(directory):
 #        os.makedirs(directory)
-
-=======
-import os
-
-account = input("Nombre de la cuenta: ")
-username = input("Nombre de usuario: ")
-password = input("Contraseña: ")
-
-print("Se grabara en la cuenta " + account + ", nombre de usuario: " + username + " password: " + password + ".")
-
-rta = input("Es correcto? [Y/n]: ")
-
-while rta not in ('Y', 'n'):
-
-    rta = input("Es correcto? [Y/n]: ")
-
-if rta == 'n':
-    quit()
-
-path = os.getcwd()
-
-carpeta = os.path.dirname(path+account)
-
-if not os.path.exists(carpeta):
-    os.makedirs(carpeta)
-
-
-
-
-#def ensure_dir(file_path):
-#    directory = os.path.dirname(file_path)
-#    if not os.path.exists(directory):
-#        os.makedirs(directory)
-
->>>>>>> 24f76d62dbfedf799817c26aa151ab6c6af97afb
